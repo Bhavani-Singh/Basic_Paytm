afee4e1aefde09252fbdc97f04acd557ff09fd13
Bhavani-Singh <bhavanisinghrajput24@gmail.com>
2024-08-18 15:11:24 +0530

    remove db call from the auth middleware and refactor response handling to use return statement with res.status().json()

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -4,25 +4,41 @@ const { User } = require("../db");
 
 async function authMiddleware(req, res, next) {
     const bearerToken = req.headers.authorization;
-    if(!bearerToken) {
-        res.status(400).json({
+    if(!bearerToken || !bearerToken.startsWith("Bearer ")) {
+
+        return res.status(403).json({
             message: "Access denied not token provided"
-        })
+        });
+        
     }
+
     const token = bearerToken.split(" ")[1];
     
-    const decoded = jwt.verify(token, jwtSecret);
+    try {
 
-    const user = await User.findById({_id: decoded.id}, "username");
+        const decoded = jwt.verify(token, jwtSecret);
     
-    if(user) {
-        req.userId = decoded.id;
-        next();
+        if(decoded) {
+
+            req.userId = decoded.id;
+            next();
+
+        }
+        else {
+
+            res.status(403).json({
+                message: "Invalid token"
+            });
+
+        }
+
     }
-    else {
-        res.status(403).json({
-            message: "Invalid token"
-        })
+    catch(error) {
+
+        return res.status(403).json({
+            message: "Error while authenticating the user"
+        });
+        
     }
 
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -27,10 +27,11 @@ router.post("/signup", async (req, res) => {
             const userAlreadyExist = await User.findOne({username});
 
             if(userAlreadyExist) {
-                res.status(411).json({
+
+                return res.status(411).json({
                     message: "user already exist"
-                })
-                return;
+                });
+
             }
 
             const user = await User.create({
@@ -41,39 +42,40 @@ router.post("/signup", async (req, res) => {
                             });
 
             if(user) {
-            
+
                 const id = user.id;
                 let token = jwt.sign({id}, jwtSecret);
                 token = "Bearer " + token;
 
-                res.status(201).json({
+                return res.status(201).json({
                     token,
                     message: "User created successfully"
                 });
 
-                return;
             }
             else {
-                res.status(500).json({
+
+                return res.status(500).json({
                     message: "Error while creating user"
                 });
 
-                return;
             }
         }
         catch(err) {
-            res.status(500).json({
+
+            return res.status(500).json({
                 message: "Catch Error while creating user" + err
             });
 
-            return;
         }
         
     }
     else {
-        res.status(400).json({
+
+        return res.status(400).json({
             message: "Invalid credentials"
         });
+
     }
     
     
@@ -83,24 +85,25 @@ router.post("/signin", async (req, res) => {
     const {username, password } = req.body;
 
     try {
+
         const user = await User.findOne({username});
 
         if(!user) {
-            res.status(401).json({
+
+            return res.status(401).json({
                 message: "Invalid user name and password"
             });
-            
-            return;
+
         } 
 
         const validPassword = bcrypt.compareSync(password, user.password);
 
         if(!validPassword) {
-            res.status(401).json({
+
+            return res.status(401).json({
                 message: "Invalid user name or password"
             });
 
-            return;
         }
 
         const id = user.id;
@@ -108,18 +111,17 @@ router.post("/signin", async (req, res) => {
         let token = jwt.sign({id}, jwtSecret);
         token = "Bearer " + token;
         
-        res.status(200).json({
+        return res.status(200).json({
             token
         });
-        
-        return;
+
     }
     catch(err) {
-        res.status(411).json({
+
+        return res.status(411).json({
             message: "Error while logging in"
         });
-
-        return;
+        
     }
 });
 
